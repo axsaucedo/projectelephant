16677393891318cb68295532f1cfb3a83c5dad74
Prathap <notprathap@gmail.com>
2013-06-08 23:55:02 +0100

    random number

--- Server/code/src/Server/ApiBundle/Controller/DefaultController.php
+++ Server/code/src/Server/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace Server\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ServerApiBundle:Default:index.html.twig', array('name' => 'Elephant'));
+        //return $this->render('ServerApiBundle:Default:index.html.twig', array('name' => rand()));
+        return new JsonResponse(rand());
     }
 }

f934cce6d7d6dcf47346b01ce630031cbbf8fae1
Prathap <notprathap@gmail.com>
2013-06-09 01:02:48 +0100

    Stubs for api calls

--- Server/code/src/Server/ApiBundle/Controller/DefaultController.php
+++ Server/code/src/Server/ApiBundle/Controller/DefaultController.php
@@ -7,9 +7,19 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function initiateAction()
     {
         //return $this->render('ServerApiBundle:Default:index.html.twig', array('name' => rand()));
         return new JsonResponse(rand());
+    }
+    
+    public function getMessageAction()
+    {
+        return new JsonResponse('super');
+    }
+    
+    public function postMessageAction()
+    {
+        return new JsonResponse('ok');
     }
 }

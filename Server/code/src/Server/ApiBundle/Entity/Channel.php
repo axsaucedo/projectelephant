e38d45dc3bf74aa2bb83118cc179e2ce781e490a
axsauze <axsauze@gmail.com>
2013-06-09 01:27:59 +0100

    Finally added that annoying table

--- Server/code/src/Server/ApiBundle/Entity/Channel.php
+++ Server/code/src/Server/ApiBundle/Entity/Channel.php
@@ -10,25 +10,25 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="channel")
  */
 class Channel  {
-	/*
-	* @ORM|Id
-	* @ORM|Column(type="integer")
-	* @ORM|GeneratedValue(strategy="AUTO")
+	/**
+	* @ORM\Id
+	* @ORM\Column(type="integer")
+	* @ORM\GeneratedValue(strategy="AUTO")
 	*/
 	protected $messageid;
 
-	/*
-	* @ORM|Column(type="string", length=50)
+	/**
+	* @ORM\Column(type="string", length=50)
 	*/
 	protected $name;
 
-	/*
-	* @ORM|Column(type="string", length=255)
+	/**
+	* @ORM\Column(type="string", length=255)
 	*/
 	protected $message;
 
-	/*
-	* @ORM|Column(type="integer")
+	/**
+	* @ORM\Column(type="integer")
 	*/
 	protected $channel;
 }

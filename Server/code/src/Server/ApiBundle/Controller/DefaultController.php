e9f6b497d53e5e9bb3df8899a020b3dec8df264f
Prathap <notprathap@gmail.com>
2013-06-09 02:08:01 +0100

    Partially working update

--- Server/code/src/Server/ApiBundle/Controller/DefaultController.php
+++ Server/code/src/Server/ApiBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Server\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Server\ApiBundle\Entity\Channel;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,8 +15,24 @@ class DefaultController extends Controller
         return new JsonResponse(rand());
     }
     
-    public function postMessageAction()
+    public function postMessageAction(Request $request)
     {
-        return new JsonResponse('ok');
+        $name = $request->request->get('name', 'anon');
+        $message = $request->request->get('message', '');
+        $channel = $request->request->get('channel', 'anon');
+        
+        $objChannel = new Channel();
+        $objChannel->setName($name);
+        $objChannel->setMessage($message);
+        $objChannel->setChannel($channel);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($objChannel);
+        $em->flush();
+        
+        $repository = $this->getDoctrine()->getRepository("ServerApiBundle:Channel");
+        $messages = $repository->findByChannel($channel);
+        
+        return new JsonResponse($messages);
     }
 }
